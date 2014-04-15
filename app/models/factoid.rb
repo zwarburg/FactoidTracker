b83b6be17846a4757b7f783a4ed3926a942c2195
Zack Warburg <Zack.warburg@appfolio.com>
2014-04-15 11:06:47 -0700

    zw - adding dropdown for names

--- app/models/factoid.rb
+++ app/models/factoid.rb
@@ -4,4 +4,6 @@ class Factoid < ActiveRecord::Base
   validates_presence_of :description, :name, :title
   validates_uniqueness_of :title
 
+
+  NAMES = "Angela", "Geordie", "Jared", "Jennifer", "Kevin", "Matthew", "Oscar", "Owen", "Regina", "Todd", "Vaibhavi", "Zack"
 end

44ef26909f1a3993dd419e65a1d8a9b6cd14bae3
Zack Warburg <Zack.warburg@appfolio.com>
2014-04-22 16:21:34 -0700

    zw - setting up test-unit

--- test/functional/factoids_controller_test.rb
+++ test/functional/factoids_controller_test.rb
@@ -1,22 +1,22 @@
 require 'test_helper'
 
 class FactoidsControllerTest < ActionController::TestCase
-  setup do
+  def setup
     @factoid = factoids(:one)
   end
 
-  test "should get index" do
+  def test_index
     get :index
     assert_response :success
     assert_not_nil assigns(:factoids)
   end
 
-  test "should get new" do
+  def test_new
     get :new
     assert_response :success
   end
 
-  test "should create factoid" do
+  def test_create
     assert_difference('Factoid.count') do
       post :create, factoid: { description: "Description", name: "Zack", title: "My Factoid" }
     end
@@ -24,22 +24,44 @@ class FactoidsControllerTest < ActionController::TestCase
     assert_redirected_to factoid_path(assigns(:factoid))
   end
 
-  test "should show factoid" do
+  def test_create__fails_without_title
+    f = Factoid.new(:description => "My Awesome Factoid", :name => "Zack")
+    assert_false f.save
+  end
+
+  def test_create__fails_without_description
+    f = Factoid.new(:title => "Title", :name => "Zack")
+    assert_false f.save
+    end
+
+  def test_create__fails_without_name
+    f = Factoid.new(:title => "Title", :description => "My Awesome Factoid", :name => "Zack")
+    assert_false f.save
+  end
+
+  def test_create__fails_for_non_unique
+    f1 = Factoid.new(:title => "Title", :description => "My Awesome Factoid", :name => "Zack")
+    assert f1.save
+    f2 = Factoid.new(:title => "Title", :description => "My Awesome Factoid", :name => "Zack")
+    assert_false f2.save
+  end
+
+  def test_show
     get :show, id: @factoid
     assert_response :success
   end
 
-  test "should get edit" do
+  def test_edit
     get :edit, id: @factoid
     assert_response :success
   end
 
-  test "should update factoid" do
+  def test_update
     put :update, id: @factoid, factoid: { description: @factoid.description, name: @factoid.name, title: @factoid.title }
     assert_redirected_to factoid_path(assigns(:factoid))
   end
 
-  test "should destroy factoid" do
+  def test_destroy
     assert_difference('Factoid.count', -1) do
       delete :destroy, id: @factoid
     end
